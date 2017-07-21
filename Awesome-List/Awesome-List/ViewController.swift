08844d97bf8749302bd665f5395e254b97e93d8d
Panda Maru <pandamaru1331@gmail.com>
2017-07-21 17:16:35 +0700

    Init project.

---
+++ Awesome-List/Awesome-List/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Awesome-List
+//
+//  Created by PandaMaru on 7/21/2560 BE.
+//  Copyright © 2560 amuse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
